b96b78e51100f52a379ac3862f5bf81b3252ac86
itzenghao <itzenghao@163.com>
2016-10-24 22:55:42 +0800

    闪屏页面，引导页面，+主页 Fragment 切换，快速开发必备…

---
+++ app/src/main/java/com/phoenix/base/HomePage.java
@@ -0,0 +1,71 @@
+package com.phoenix.base;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import com.phoenix.base.bean.Tab;
+import com.phoenix.base.fragment.CartFragment;
+import com.phoenix.base.fragment.CategoryFragment;
+import com.phoenix.base.fragment.HomeFragment;
+import com.phoenix.base.fragment.HotFragment;
+import com.phoenix.base.fragment.MineFragment;
+import com.phoenix.base.weiget.FragmentTabHost;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomePage extends AppCompatActivity {
+    private FragmentTabHost mTabhost;
+    private LayoutInflater mInflater;
+    private List<Tab> mTabs = new ArrayList<>(5);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+
+        initTab();
+    }
+
+    private void initTab() {
+        Tab tab_home = new Tab(R.string.home, R.drawable.selector_icon_home, HomeFragment.class);
+        Tab tab_hot = new Tab(R.string.hot, R.drawable.selector_icon_hot, HotFragment.class);
+        Tab tab_category = new Tab(R.string.catagory, R.drawable.selector_icon_category, CategoryFragment.class);
+        Tab tab_cart = new Tab(R.string.cart, R.drawable.selector_icon_cart, CartFragment.class);
+        Tab tab_mine = new Tab(R.string.mine, R.drawable.selector_icon_mine, MineFragment.class);
+
+        mTabs.add(tab_home);
+        mTabs.add(tab_hot);
+        mTabs.add(tab_category);
+        mTabs.add(tab_cart);
+        mTabs.add(tab_mine);
+
+        mInflater = LayoutInflater.from(this);
+        mTabhost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabhost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        for (Tab tab : mTabs){
+            TabHost.TabSpec tabSpec = mTabhost.newTabSpec(getString(tab.getTitle()));
+            tabSpec.setIndicator(buildIndicator(tab));
+            mTabhost.addTab(tabSpec, tab.getFragment(), null);
+        }
+        mTabhost.getTabWidget().setShowDividers(LinearLayout.SHOW_DIVIDER_NONE);
+        mTabhost.setCurrentTab(0);
+    }
+
+    private View buildIndicator(Tab tab){
+        View view = mInflater.inflate(R.layout.tab_indicator, null);
+        ImageView img = (ImageView) view.findViewById(R.id.icon_tab);
+        TextView text = (TextView) view.findViewById(R.id.txt_indicator);
+
+        img.setBackgroundResource(tab.getIcon());
+        text.setText(tab.getTitle());
+        return view;
+    }
+}
---
+++ app/src/main/java/com/phoenix/base/fragment/CategoryFragment.java
@@ -0,0 +1,22 @@
+package com.phoenix.base.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.phoenix.base.R;
+
+/**
+ * Created by flashing on 2016/6/9.
+ */
+public class CategoryFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        return view;
+    }
+}
